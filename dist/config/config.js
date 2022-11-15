63968a01c4f88b5d6e0102bce5d782b6ed8b155c
Aldo Capezzali <aldocape@gmail.com>
2022-11-14 21:03:14 -0300

    hago build

--- dist/config/config.js
+++ dist/config/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isAdmin = exports.PORT = void 0;
 // Utilizo el puerto de escucha 8080 para desarrollo y process.env.PORT para producción en glitch.com
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 exports.PORT = PORT;
 // Variable booleana para indicar si el usuario es o no un Administrador
 const isAdmin = true;
