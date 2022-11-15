113c6dd305920a2e9478ef04c3bd4321f468652b
Aldo Capezzali <aldocape@gmail.com>
2022-11-14 21:01:32 -0300

    hago build

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 // Utilizo el puerto de escucha 8080 para desarrollo y process.env.PORT para producción en glitch.com
-const PORT: number | string = process.env.PORT || 4000;
+const PORT: number | string = process.env.PORT || 8080;
 
 // Variable booleana para indicar si el usuario es o no un Administrador
 const isAdmin: boolean = true;
